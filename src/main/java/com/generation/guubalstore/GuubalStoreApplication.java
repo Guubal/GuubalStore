91790854ce2b95651d0d944d4ad9058baa18312c
Gustavo Balieiro <gbalieirof@gmail.com>
2023-02-27 23:26:32 -0300

    Primeiro Commit

---
+++ src/main/java/com/generation/guubalstore/GuubalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.generation.guubalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuubalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuubalStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/guubalstore/GuubalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.guubalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuubalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
